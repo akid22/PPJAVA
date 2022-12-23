c0694273ab93a56bfbbb03936abf6a1a03009420
akid22 <121268320+akid22@users.noreply.github.com>
2022-12-23 02:57:15 +0200

    Add files via upload

---
+++ Calc/src/main/java/com/example/calc/Calculator.java
@@ -0,0 +1,24 @@
+package com.example.calc;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Calculator extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Calculator");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
